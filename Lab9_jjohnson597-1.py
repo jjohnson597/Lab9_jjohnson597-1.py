100aecea2268b8ce53359617c3e78617483c4f67
Jaylen Johnson <jjohnson597@student.cscc.edu>
2026-06-28 16:33:57 -0400

    Add: Create players and display starting wallets

--- Lab9_jjohnson597-1.py
+++ Lab9_jjohnson597-1.py
@@ -8,3 +8,11 @@ Date: 6/28/2026
 
 from player import Player
 
+def main():
+    """Run the Match Coins game."""
+    player1 = Player("Player 1")
+    player2 = Player("Player 2")
+
+    print("--- Coin Match Game ---")
+    print(f"{player1.get_name()} has {player1.get_wallet()} coins.")
+    print(f"{player2.get_name()} has {player2.get_wallet()} coins.")
