fe6f1e6dc6fbdb124f81ce79800f5a749ba624e1
Jaylen Johnson <jjohnson597@student.cscc.edu>
2026-06-28 16:07:47 -0400

    Add: Create Coin class with private side attribute

---
+++ coin.py
@@ -0,0 +1,16 @@
+"""
+Program: Match Coins Game - Coin Class
+Author: Jaylen Johnson
+Purpose: Represents a single coin that can be tossed.
+Starter code: No starter code was used.
+Date: 6/28/2026
+"""
+
+import random
+
+class Coin:
+    """Represents a single coin."""
+
+    def __init__(self):
+        """Initialize the coin with a random starting side."""
+        self.__sideup = random.choice(["Heads", "Tails"])
