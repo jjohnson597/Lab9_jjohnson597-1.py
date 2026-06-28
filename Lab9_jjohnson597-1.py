4086049cde64e63308a0621e23645a7a8bdd2ce6
Jaylen Johnson <jjohnson597@student.cscc.edu>
2026-06-28 16:30:58 -0400

    Create: Add main game header and Player import

---
+++ Lab9_jjohnson597-1.py
@@ -0,0 +1,10 @@
+"""
+Program: Match Coins Game
+Author: Jaylen Johnson
+Purpose: Runs a two-player coin matching game using Player objects.
+Starter code: No starter code was used.
+Date: 6/28/2026
+"""
+
+from player import Player
+
