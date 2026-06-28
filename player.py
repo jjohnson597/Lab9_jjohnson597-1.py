5657ecde5ca10023f2f6c48e4615f462dea52f10
Jaylen Johnson <jjohnson597@student.cscc.edu>
2026-06-28 16:14:07 -0400

    Create: Add Player class header and Coin import

---
+++ player.py
@@ -0,0 +1,9 @@
+"""
+Program: Match Coins Game - Player Class
+Author: Jaylen Johnson
+Purpose: Represents a player with a name, wallet, and coin object.
+Starter code: No starter code was used.
+Date: 6/28/2026
+"""
+
+from coin import Coin
